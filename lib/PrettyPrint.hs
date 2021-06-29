b843776294cb08cb2c2229d78a183abc06019af0
cutsea110 <cutsea110@gmail.com>
2021-06-29 14:21:35 +0900

    refactor by hls advices

--- lib/PrettyPrint.hs
+++ lib/PrettyPrint.hs
@@ -32,8 +32,8 @@ iDisplay seq      = flatten 0 [(seq, 0)]
 iNum n            = iStr (show n)
 iFWNum width n    = iStr (space (width - length digits) ++ digits)
   where digits = show n
-iLayn seqs        = iConcat (map lay_item (zip [1..] seqs))
-  where lay_item (n, seq)
+iLayn seqs        = iConcat (zipWith lay_item [1..] seqs)
+  where lay_item n seq
           = iConcat [ iFWNum 4 n, iStr ") ", iIndent seq, iNewline ]
 
 flatten :: Int                   -- ^ Current column; 0 for first column
@@ -49,10 +49,10 @@ flatten col ((IAppend seq1 seq2, indent):seqs)
 flatten col ((IIndent seq, indent):seqs)
   = flatten col ((seq, col):seqs)
 flatten col ((INewline, indent):seqs)
-  = '\n' : (space indent) ++ (flatten indent seqs)
+  = '\n' : space indent ++ flatten indent seqs
 
 space :: Int -> String
-space n = take n (repeat ' ')
+space n = replicate n ' '
 
 pprExpr :: CoreExpr -> Iseq
 pprExpr (ENum n) = iStr (show n)
@@ -60,7 +60,7 @@ pprExpr (EVar v) = iStr v
 pprExpr (EAp (EAp (EVar op) e1) e2)
   | op `elem` ["+", "*", "-", "/", "<", ">"]
   = iInterleave (iStr " ") [ pprAExpr e1, iStr op, pprAExpr e2 ]
-pprExpr (EAp e1 e2) = (pprExpr e1) `iAppend` (iStr " ") `iAppend` (pprAExpr e2)
+pprExpr (EAp e1 e2) = pprExpr e1 `iAppend` iStr " " `iAppend` pprAExpr e2
 pprExpr (ELet isrec defns expr)
   = iConcat [ iStr keyword, iNewline
             , iStr "  ", iIndent (pprDefns defns), iNewline
@@ -81,7 +81,7 @@ pprDefn (name, expr)
   = iConcat [ iStr name, iStr " = ", iIndent (pprExpr expr) ]
 
 iConcat :: [Iseq] -> Iseq
-iConcat seqs = foldl iAppend iNil seqs
+iConcat = foldl iAppend iNil
 
 iInterleave :: Iseq -> [Iseq] -> Iseq
 iInterleave sep seqs = iConcat (intersperse sep seqs)
