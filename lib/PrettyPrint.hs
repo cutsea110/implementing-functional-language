1fc39c58bb5a966d0bc0868315dfdcdc88e2cd1f
cutsea110 <cutsea110@gmail.com>
2021-06-29 14:39:18 +0900

    add new infix operators

--- lib/PrettyPrint.hs
+++ lib/PrettyPrint.hs
@@ -18,38 +18,32 @@ iStr     :: String -> Iseq                   -- ^ Turn a string into an iseq
 iAppend  :: Iseq -> Iseq -> Iseq             -- ^ Append two iseqs
 iNewline :: Iseq                             -- ^ New line with indentation
 iIndent  :: Iseq -> Iseq                     -- ^ Indent an iseq
-iDisplay :: Iseq -> String                   -- ^ Turn an iseq into a string
 iNum     :: Int -> Iseq                      -- ^ Trun a num into an iseq
 iFWNum   :: Int -> Int -> Iseq               -- ^ Same as iNum with left-padded spaces
 iLayn    :: [Iseq] -> Iseq                   -- ^ Lays out a list, numbering the items
+iDisplay :: Iseq -> String                   -- ^ Turn an iseq into a string
 
 iNil              = INil
 iStr str          = IStr str
 iAppend seq1 seq2 = IAppend seq1 seq2
 iNewline          = INewline
 iIndent seq       = IIndent seq
-iDisplay seq      = flatten 0 [(seq, 0)]
 iNum n            = iStr (show n)
 iFWNum width n    = iStr (space (width - length digits) ++ digits)
   where digits = show n
 iLayn seqs        = iConcat (zipWith lay_item [1..] seqs)
-  where lay_item n seq
-          = iConcat [ iFWNum 4 n, iStr ") ", iIndent seq, iNewline ]
+  where lay_item n seq = iConcat [ iFWNum 4 n, iStr ") ", iIndent seq, iNewline ]
+iDisplay seq      = flatten 0 [(seq, 0)]
 
 flatten :: Int                   -- ^ Current column; 0 for first column
         -> [(Iseq, Int)]         -- ^ Work list
         -> String                -- ^ Result
-flatten col [] = ""
-flatten col ((INil, indent):seqs)
-  = flatten indent seqs
-flatten col ((IStr s, indent):seqs)
-  = s ++ flatten (length s + indent) seqs
-flatten col ((IAppend seq1 seq2, indent):seqs)
-  = flatten indent ((seq1, indent):(seq2, indent):seqs)
-flatten col ((IIndent seq, indent):seqs)
-  = flatten col ((seq, col):seqs)
-flatten col ((INewline, indent):seqs)
-  = '\n' : space indent ++ flatten indent seqs
+flatten col []                                 = ""
+flatten col ((INil, indent):seqs)              = flatten indent seqs
+flatten col ((IStr s, indent):seqs)            = s ++ flatten (length s + indent) seqs
+flatten col ((IAppend seq1 seq2, indent):seqs) = flatten indent ((seq1, indent):(seq2, indent):seqs)
+flatten col ((IIndent seq, indent):seqs)       = flatten col ((seq, col):seqs)
+flatten col ((INewline, indent):seqs)          = '\n' : space indent ++ flatten indent seqs
 
 space :: Int -> String
 space n = replicate n ' '
